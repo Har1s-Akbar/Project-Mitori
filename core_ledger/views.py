a79cd60907528b8bcad749822a4c8edba6a36915
Har1s-Akbar <stanker801@gmail.com>
2026-07-06 20:04:35 +0500

    creating views, serializers , changing models and making the project secure commit-2

--- core_ledger/views.py
+++ core_ledger/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from serializers import PortfolioSerializer,PositionSerializer,LedgerTransactionSerializer
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from .models import Portfolio,LedgerTransaction,Position
+
+
+class PortfolioView(generics.RetrieveAPIView):
+    serializer_class = PortfolioSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.portfolio
+    
+class PositionsView(generics.ListAPIView):
+    serializer_class = PositionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Position.objects.filter(portfolio= self.request.user.portfolio)
+    
+class LedgerView(generics.ListAPIView):
+    serializer_class= LedgerTransactionSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return LedgerTransaction.objects.filter(portfolio = self.request.user.portfolio).order_by('-time_stamp')
+    
