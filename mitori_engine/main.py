c712f4c769ef775a6dff7a12796728b8e62a2fc0
Har1s-Akbar <stanker801@gmail.com>
2026-07-13 17:36:09 +0500

    mitori_engine configuration main.py figuring routes and -1

--- mitori_engine/main.py
+++ mitori_engine/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 import uvicorn
+from .core.models import Side, Order
+from .core.engine import OrderBook
 
 app = FastAPI(
     title ="mitori-engine",
@@ -7,13 +10,41 @@ app = FastAPI(
     version = "1.0.0"
 )
 
-@app.get("/welcome")
-async def welcome():
-    
+MARKET ={
+    "APP": OrderBook("APP"),
+    "TSLA": OrderBook("TSLA"),
+    "AUX":OrderBook("AUX")
+}
+
+class OrderReq(BaseModel):
+    ticker:str = Field(min_length=1, max_length=10, title="Ticker", description="Ticker is required", strict=True)
+    side:Side = Field(title="side" , description="Side is required", strict=True)
+    price:float = Field(ge=1, lt=1000000000.0, allow_inf_nan=False, strict=True)
+    number_of_shares:int = Field(ge=1, lt=2000, allow_inf_nan=False, strict=True)
+
+@app.post("/order")
+async def place_order(order:OrderReq):
+    if order.ticker not in MARKET:
+        raise HTTPException(status_code=404,detail="Ticker does not exist")
+    target_book = MARKET[order.ticker]
+
+    new_order = Order(
+        ticker =  order.ticker,
+        side = order.side,
+        price = order.price,
+        number_of_shares = order.number_of_shares
+    )
+
+    target_book.add_order(new_order)
+    executed_trades = target_book.execute()
+
     return {
-        "status" : "200",
-        "message":"Welcome message for everyone trying to access mitori"
+        "message":"Order Accepted",
+        "trades":executed_trades
     }
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
