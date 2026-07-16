ce991985e569dad631f376a8aa7554f381942e8e
Har1s-Akbar <stanker801@gmail.com>
2026-07-16 16:30:15 +0500

    configuring redis for fire and persist more info about the choice will be given in readme -1

---
+++ mitori_engine/infrastructure/client.py
@@ -0,0 +1,17 @@
+import asyncio
+import redis.asyncio as redis
+from redis.asyncio import BlockingConnectionPool
+
+def create_redis_pool () -> BlockingConnectionPool:
+    return BlockingConnectionPool(
+        host = "localhost",
+        port = 6379,
+        db = 0,
+        decode_responses=True,
+        max_connections=15,
+        socket_connect_timeout=2.0,
+        socket_timeout=5.0,
+        retry_on_timeout=True,
+        health_check_interval=30,
+    )
+
