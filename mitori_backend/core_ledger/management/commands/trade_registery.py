821cfa5b9f30ebc8b1ae3c6faaa69079531631de
Har1s-Akbar <stanker801@gmail.com>
2026-07-16 23:02:08 +0500

    custom management commands configuration with redis -2

--- mitori_backend/core_ledger/management/commands/trade_registery.py
+++ mitori_backend/core_ledger/management/commands/trade_registery.py
@@ -1,7 +1,19 @@
 from django.core.management.base import BaseCommand
+import redis
 
 class Command(BaseCommand):
     help = "Custom Daemon for registering trades in postgres"
 
     def handle(self, *args,**options):
-        print('This is a hello from the daemon')
+
+        redis_server = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+        stream_name = "executed_trades_stream"
+        group_name = "django_workers"
+
+        try:
+            redis_server.xgroup_create(name=stream_name,groupname=group_name,id=0,mkstream=False)
+            redis_server.std.out(self.style.SUCCESS(f"Created with consumer group {group_name}"))
+        except redis.exceptions.ResponseError as e:
+            if "BUSYGROUP Consumer Group name already exists" not in str(e):
+                raise e
+        self.stf.out(self.style.SUCCESS(f"Starting Streaming consumer loop"))
