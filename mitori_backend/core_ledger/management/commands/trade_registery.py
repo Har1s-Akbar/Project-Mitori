724e95e5dc586d368a8b5a49f2777d26c2dc1e67
Har1s-Akbar <stanker801@gmail.com>
2026-07-16 23:35:04 +0500

    configuring custom management command reading the fastapi stream messages through django custom command -3

--- mitori_backend/core_ledger/management/commands/trade_registery.py
+++ mitori_backend/core_ledger/management/commands/trade_registery.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 import redis
+import time
 
 class Command(BaseCommand):
     help = "Custom Daemon for registering trades in postgres"
@@ -9,11 +10,28 @@ class Command(BaseCommand):
         redis_server = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
         stream_name = "executed_trades_stream"
         group_name = "django_workers"
-
+        worker_name = "django_database_worker"
         try:
             redis_server.xgroup_create(name=stream_name,groupname=group_name,id=0,mkstream=False)
-            redis_server.std.out(self.style.SUCCESS(f"Created with consumer group {group_name}"))
+            self.stdout(self.style.SUCCESS(f"Created with consumer group {group_name}"))
         except redis.exceptions.ResponseError as e:
             if "BUSYGROUP Consumer Group name already exists" not in str(e):
                 raise e
-        self.stf.out(self.style.SUCCESS(f"Starting Streaming consumer loop"))
+        self.stdout.write(self.style.SUCCESS(f"Starting Streaming consumer loop"))
+        while True:
+            try:
+
+                executed_trades = redis_server.xreadgroup(groupname=group_name,consumername=worker_name, streams={stream_name:'>'},block=3000)
+                if executed_trades:
+                    for stream_key,messages in executed_trades:
+                        for ticker , data in messages:
+                            print(f"{ticker} with data {data}")
+                time.sleep(0.1)
+
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(f"error : {e}"))
+                time.sleep(5)
+
+
+if __name__ == "__tade_registery__":
+    Command()
