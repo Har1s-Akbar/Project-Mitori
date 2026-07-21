e700536bcb27eb8318653a31a7290618088da1eb
Har1s-Akbar <stanker801@gmail.com>
2026-07-22 00:37:39 +0500

    implementing redis pipeline and atomic transactions to beat race condition in positions | fastapi -4

--- mitori_engine/api/have_funds.py
+++ mitori_engine/api/have_funds.py
@@ -6,6 +6,7 @@ from api.security import is_user_Authenticated, AuthenticatedUser
 from fastapi import Depends
 from schemas.schema import OrderReq
 from decimal import Decimal
+import redis.exceptions as exp
 
 async def have_funds(request:Request,user:AuthenticatedUser=Depends(is_user_Authenticated) ,order:OrderReq=None ):
     order_side = order.side
@@ -14,39 +15,57 @@ async def have_funds(request:Request,user:AuthenticatedUser=Depends(is_user_Auth
     order_ticker = order.ticker
     order_user_id = user.user_id
 
+    retry_counter = 3
+
     redis_connection_port = request.app.state.redis
 
-    if order_side == "buy":
-        available_cash = await redis_connection_port.hget(f'cache:portfolio:{order_user_id}','available_cash')
-        locked_cash = await redis_connection_port.hget(f'cache:portfolio:{order_user_id}','locked_balance')
-        safe_available_cash = Decimal(available_cash or 0)
-        safe_locked_cash = Decimal(locked_cash or 0)
-        total = order_quantity*order_price
-        if total > safe_available_cash:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough funds available for this trade")
-        else:
-            safe_available_cash -= total
-            safe_locked_cash += total
-            updates={
-                'available_cash' : str(safe_available_cash),
-                'locked_balance' : str(safe_locked_cash)
-            }
-            await redis_connection_port.hset(f'cache:portfolio:{order_user_id}', mapping=updates)
+    async with redis_connection_port.pipeline() as pipeline:
+        while retry_counter>0:
+            try:
+                if order_side == "buy":
+                    await pipeline.watch(f'cache:portfolio:{order_user_id}')
+                    available_cash = await pipeline.hget(f'cache:portfolio:{order_user_id}','available_cash')
+                    locked_cash = await pipeline.hget(f'cache:portfolio:{order_user_id}','locked_balance')
+                    safe_available_cash = Decimal(available_cash or 0)
+                    safe_locked_cash = Decimal(locked_cash or 0)
 
-    if order_side == "sell":
-        available_shares = await redis_connection_port.hget(f'cache:position:{order_user_id}', order_ticker)
-        locked_shares = await redis_connection_port.hget(f'cache:position:{order_user_id}', f'locked_{order_ticker}')
+                    total = order_quantity*order_price
+                    if total > safe_available_cash:
+                        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough funds available for this trade")
+                    else:
+                        safe_available_cash -= total
+                        safe_locked_cash += total
+                        updates={
+                            'available_cash' : str(safe_available_cash),
+                            'locked_balance' : str(safe_locked_cash)
+                        }
+                        pipeline.multi()
+                        pipeline.hset(f'cache:portfolio:{order_user_id}', mapping=updates)
+                        await pipeline.execute()
+                        break
+                if order_side == "sell":
+                    await pipeline.watch(f'cache:position:{order_user_id}')
+                    available_shares = await pipeline.hget(f'cache:position:{order_user_id}', order_ticker)
+                    locked_shares = await pipeline.hget(f'cache:position:{order_user_id}', f'locked_{order_ticker}')
 
-        safe_available_shares = int(available_shares or 0)
-        safe_locked_shares = int(locked_shares or 0)
-        if order_quantity > safe_available_shares:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough shares available for trade")
-        else:
-            safe_available_shares -= order_quantity
-            safe_locked_shares += order_quantity
+                    safe_available_shares = int(available_shares or 0)
+                    safe_locked_shares = int(locked_shares or 0)
+                    if order_quantity > safe_available_shares:
+                        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough shares available for trade")
+                    else:
+                        safe_available_shares -= order_quantity
+                        safe_locked_shares += order_quantity
 
-            updates ={
-                f'{order_ticker}' : safe_available_shares,
-                f'locked_{order_ticker}' : safe_locked_shares, 
-            }
-            await redis_connection_port.hset(f'cache:position:{order_user_id}', mapping=updates)
+                        updates ={
+                            f'{order_ticker}' : safe_available_shares,
+                            f'locked_{order_ticker}' : safe_locked_shares, 
+                        }
+                        pipeline.multi()
+                        pipeline.hset(f'cache:position:{order_user_id}', mapping=updates)
+                        await pipeline.execute()
+                        break
+            except exp.WatchError:
+                retry_counter -=1
+                if retry_counter == 0:
+                    raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Retry your order in few seconds")
+                continue
