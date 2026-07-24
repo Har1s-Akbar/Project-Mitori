eb708db3e21a4950bcc0c54832f76cffaad0dc7a
Har1s-Akbar <stanker801@gmail.com>
2026-07-24 22:22:13 +0500

    tombstone deletion for canceled order -2 | fastApi

--- mitori_engine/core/engine.py
+++ mitori_engine/core/engine.py
@@ -9,22 +9,31 @@ class OrderBook():
         self.bid =[]
         self.ask=[]
         self.ticker = ticker
-        self.active_uuids = set()
-
+        self.active_uuids = {}
+        self.canceled_uuids = set()
     def add_order(self, order:Order):
         if(order.side == Side.SELL):
             sorted_tuple = (order.price, order.date_time,order.order_id, order)
             heapq.heappush(self.ask, sorted_tuple)
-            
+            self.active_uuids[order.order_id] = order
         if(order.side == Side.BUY):
             sorted_tuple = (-1*(order.price), order.date_time,order.order_id, order)
             heapq.heappush( self.bid,sorted_tuple)
+            self.active_uuids[order.order_id] = order
 
     def execute(self):
         trades_executed = []
         while self.bid and self.ask :
             best_bid = self.bid[0][3]
             best_ask = self.ask[0][3]
+            if best_bid.order_id in self.canceled_uuids:
+                heapq.heappop(self.ask)
+                self.canceled_uuids.remove(best_bid.order_id)
+                continue
+            if best_ask.order_id in self.canceled_uuids:
+                heapq.heappop(self.bid)
+                self.canceled_uuids.remove(best_ask.order_id)
+                continue
             if(best_bid.price < best_ask.price):
                 break
             if(best_bid.price >= best_ask.price):
@@ -35,8 +44,17 @@ class OrderBook():
 
             if best_ask.is_filled:
                 heapq.heappop(self.ask)
+                self.active_uuids.pop(best_ask.order_id, None)
             if best_bid.is_filled:
                 heapq.heappop(self.bid)
+                self.active_uuids.pop(best_bid,None)
         return trades_executed
 
-    # def find_item(self, orde
+    def tombstone_delete(self, order_uuuid):
+        order_delete = self.active_uuids.pop(order_uuuid, None)
+        if order_delete:
+            order_delete.is_canceled = True
+            self.canceled_uuids.add(order_uuuid)
+            return True
+        else:
+            return False
