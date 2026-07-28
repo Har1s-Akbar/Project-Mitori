aeee800c19cde4e229efa6d5f45c724940237eed
Har1s-Akbar <stanker801@gmail.com>
2026-07-28 15:54:20 +0500

    found reinitialization/state overwrite bug present in the cache initialization architecture

--- mitori_backend/core_ledger/test/test_services.py
+++ mitori_backend/core_ledger/test/test_services.py
@@ -27,7 +27,7 @@ class redis_positions_portfolio_test(TransactionTestCase):
         
                 
     @patch('core_ledger.services.redis_client',test_redis_client)
-    def test_cache_settlement_redis_for_no_position(self):
+    def test_cache_initialization_redis_for_no_position(self):
         redis_positions_portfolio_service(str(self.trader.id))
 
         portfolio_key = f'cache:portfolio:{self.trader.id}'
@@ -42,7 +42,7 @@ class redis_positions_portfolio_test(TransactionTestCase):
         self.assertEqual(positions_exits,0)
 
     @patch('core_ledger.services.redis_client',test_redis_client)
-    def test_cache_settlement_redis_with__positions(self):
+    def test_cache_initialization_redis_with__positions(self):
         get_trader_instance = Portfolio.objects.get(user_id=self.trader)
         Position.objects.create(portfolio=get_trader_instance,asset_symbol='APP',quantity=130,average_entry_price=160.00)
         Position.objects.create(portfolio=get_trader_instance,asset_symbol='TSLA',quantity=50,average_entry_price=90.00)
@@ -64,4 +64,34 @@ class redis_positions_portfolio_test(TransactionTestCase):
 
         self.assertEqual(float(cached_position_APP),130.00)
         self.assertEqual(float(cached_position_TSLA),50.00)
-        
+
+    @patch('core_ledger.services.redis_client',test_redis_client)
+    def test_cache_initialization_for_relogin(self):
+        redis_positions_portfolio_service(self.trader.id)
+
+        postions_key = f'cache:positions:{self.trader.id}'
+
+        portfolio_key = f'cache:portfolio:{self.trader.id}'
+
+
+        # when the user buys the cash in portfolio is locked we are locking the funds replicating that
+        locking_portfolio_cash_in_cache= test_redis_client.hincrbyfloat(portfolio_key, 'available_cash',-200)
+        adding_to_locked_portfolio_cache= test_redis_client.hincrbyfloat(portfolio_key,'locked_balance',200)
+
+        getting_cache_cash = test_redis_client.hget(portfolio_key,'available_cash')
+        getting_locked_cache_cash = test_redis_client.hget(portfolio_key,'locked_balance')
+        self.assertEqual(float(getting_cache_cash),9800)
+        self.assertEqual(float(getting_locked_cache_cash),200)
+
+        redis_positions_portfolio_service(self.trader.id)
+
+        getting_cache_cash_after_relogin = test_redis_client.hget(portfolio_key,'available_cash')
+        getting_locked_cash_after_relogin = test_redis_client.hget(portfolio_key,'locked_balance')
+
+        self.assertEqual(float(getting_cache_cash_after_relogin),9800)
+        self.assertEqual(float(getting_locked_cash_after_relogin),200)
+
+        #This test fails  it  shows that the architecture can not support relogin cache hydration
+        #If a user has an ongoing trade and for any reason he refreshes his/her browser or relogs in
+        #That trade willl exist in the system but the cached funds/funds will be reinitialized giving him the funds that are available in the postgres
+        #creating a really catastrophic bug more will be in readme
