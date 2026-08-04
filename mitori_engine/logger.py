f5e2009876c20b76d1225f0584c8ccd825864569
Har1s-Akbar <stanker801@gmail.com>
2026-08-04 14:07:36 +0500

    adding structlog and orjson lightning fast library for logging in fastapi

---
+++ mitori_engine/logger.py
@@ -0,0 +1,43 @@
+import orjson
+import structlog
+import sys
+import logging
+
+def configuer_fastapi_logging():
+    def orjson_dumps(obj, **kwargs):
+        return orjson.dumps(obj).decode('utf-8')
+
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.format_exc_info,
+    ]
+
+    structlog.configure(
+        processors=shared_processors + [
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processor=[
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.processors.JSONRenderer(serializer=orjson_dumps)
+        ],
+    )
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.handlers = [handler]
+    root_logger.setLevel= [logging.INFO]
+
+    for _log in ["uvicorn", "uvicorn.error", "uvicorn.access"]:
+        logging.getLogger(_log).handlers = [handler]
+        logging.getLogger(_log).propagate = False
