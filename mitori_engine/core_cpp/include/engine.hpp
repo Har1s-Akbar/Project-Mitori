25d23246779eaf8ce4f3efe579ea401a8b59edb6
Har1s-Akbar <stanker801@gmail.com>
2026-08-13 15:31:55 +0500

    implementing the order book and the process_order  and reset methods

--- mitori_engine/core_cpp/include/engine.hpp
+++ mitori_engine/core_cpp/include/engine.hpp
@@ -25,3 +25,20 @@ struct BidComparator {
 struct AskComparator {
     bool operator()(const Order* a, const Order* b) const;
 };
+
+class OrderBook {
+private:
+    std::priority_queue<Order*, std::vector<Order*>, BidComparator> bids;
+    std::priority_queue<Order*, std::vector<Order*>, AskComparator> asks;
+    uint64_t current_time;
+
+    void match_buy(Order* buy_order);
+    void match_sell(Order* sell_order);
+
+public:
+    OrderBook();
+
+    void process_order(Order* order);
+    
+    void reset();
+};
--- mitori_engine/core_cpp/src/engine.cpp
+++ mitori_engine/core_cpp/src/engine.cpp
@@ -27,3 +27,35 @@ bool AskComparator::operator()(const Order* a , const Order* b)const{
     }
     return a->price > b->price;
 }
+
+void OrderBook::reset(){
+    current_time = 0;
+
+    bids = std::priority_queue<Order*, std::vector<Order*>, BidComparator>();
+    asks = std::priority_queue<Order*, std::vector<Order*>, AskComparator>();
+}
+
+void OrderBook::process_order(Order* order){
+    order->timestamp = current_time++;
+    order->is_canceled = false;
+    
+    if(order->type == Type::LIMIT){
+        if(order->side == Side::BUY){
+            match_buy(order);
+            if(order->number_ofshares > 0){
+                bids.push(order);
+            }
+    }else{
+        match_sell(order);
+        if(order->number_ofshares > 0){
+            asks.push(order);
+        }
+    }
+    }else if(order->type == Type::MARKET){
+        if(order->side == Side::BUY){
+            match_buy(order);
+        }else{
+            match_sell(order);
+        }
+    }
+}
