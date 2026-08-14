558bc3cc72b1a2aed1f97aa325c89813cae81722
Har1s-Akbar <stanker801@gmail.com>
2026-08-14 17:10:25 +0500

    binding complete for c++

--- mitori_engine/core_cpp/bindings.cpp
+++ mitori_engine/core_cpp/bindings.cpp
@@ -21,11 +21,17 @@ PYBIND11_MODULE(mitori_engine, m){
         
     py::class_<Trade>(m, "Trade")
         .def_readonly("ticker", &Trade::ticker)
-        .def_readonly("quantity", &Trade::quantity)
-        .def_readonly("price_setteled_at", &Trade::price_setteled_at)
-        .def_readonly("price_locked_by_user", &Trade::price_locked_by_user)
+        .def_property_readonly("quantity", [](const Trade& t) {
+            return static_cast<double>(t.quantity) / PRICE_PRECISION;
+        })
         .def_readonly("buyer_id", &Trade::buyer_id)
-        .def_readonly("seller_id", &Trade::seller_id);
+        .def_readonly("seller_id", &Trade::seller_id)
+        .def_property_readonly("price_locked_by_user", [](const Trade& t) {
+            return static_cast<double>(t.price_locked_by_user) / PRICE_PRECISION;
+        })
+        .def_property_readonly("price_settled_at", [](const Trade& t) {
+            return static_cast<double>(t.price_setteled_at) / PRICE_PRECISION;
+        });
 
     py::class_<OrderBook>(m,"OrderBook")
         .def(py::init<std::string>())
@@ -35,8 +41,8 @@ PYBIND11_MODULE(mitori_engine, m){
                     Side side,
                     Type type,
                     bool is_canceled,
-                    uint64_t price,
-                    uint64_t number_of_shares,
+                    double price,
+                    double number_of_shares,
                     std::optional<double> max_authorized_funds){
                         Order* order = ArenaAllocator::allocate();
                         order->order_id = order_id;
@@ -46,5 +52,21 @@ PYBIND11_MODULE(mitori_engine, m){
                         order->number_of_shares = number_of_shares;
             
                         order->price = static_cast<uint64_t>(price * PRICE_PRECISION);
+                        order->number_of_shares = static_cast<uint64_t>(number_of_shares * PRICE_PRECISION); 
+                        if (max_authorized_funds.has_value()) {
+                        order->max_authorized_funds = static_cast<uint64_t>(max_authorized_funds.value() * PRICE_PRECISION);
+                        } else {
+                        order->max_authorized_funds = UINT64_MAX; 
+                        }
+                        return book.process_order(order);
         })
+        .def("get_current_bbo", [](OrderBook & book){
+            auto bbo = book.get_current_bbo();
+            std::unordered_map<std::string, double> scaled_bbo;
+            scaled_bbo["best_ask_price"] = static_cast<double>(bbo["best_ask_price"]) / PRICE_PRECISION;
+            scaled_bbo["best_bid_price"] = static_cast<double>(bbo["best_bid_price"]) / PRICE_PRECISION;
+            return scaled_bbo;
+        });
+        m.def("reset_memory", &ArenaAllocator::reset, "Zero-latency allocator reset");
+        m.def("cleanup_memory", &ArenaAllocator::cleanup, "Safely destroy memory slabs");
 }
