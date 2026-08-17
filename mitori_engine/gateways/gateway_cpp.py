a1bcf8364325e14c35f16ae57b3128a3ce3f858f
Har1s-Akbar <stanker801@gmail.com>
2026-08-18 01:31:26 +0500

    fixixng the test

--- mitori_engine/gateways/gateway_cpp.py
+++ mitori_engine/gateways/gateway_cpp.py
@@ -39,7 +39,7 @@ class MitoriGateway:
 
         price_scaled = int(price * self.PRECISION_MULTIPLIER) if price is not None else 0
         shares_scaled = int(number_of_shares * self.PRECISION_MULTIPLIER)
-        max_funds_scaled = int(max_authorized_funds * self.PRECISION_MULTIPLIER) if max_authorized_funds is not None  else 0
+        max_funds_scaled = int(max_authorized_funds * self.PRECISION_MULTIPLIER) if max_authorized_funds is not None  else None
 
         raw_trades = self.book.process_order(
             order_id_high=oid_high,
