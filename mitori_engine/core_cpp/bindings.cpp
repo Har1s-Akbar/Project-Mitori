d721ae9821c578f58e24e042b48e049a24c532d3
Har1s-Akbar <stanker801@gmail.com>
2026-08-26 23:06:51 +0500

    fixing the typo in binding

--- mitori_engine/core_cpp/bindings.cpp
+++ mitori_engine/core_cpp/bindings.cpp
@@ -186,5 +186,5 @@ PYBIND11_MODULE(mitori_engine_cpp, m){
         )
         .def("get_current_bbo", &OrderBook::get_current_bbo, py::call_guard<py::gil_scoped_release>())
         .def("reset_engine", &OrderBook::reset_engine, py::call_guard<py::gil_scoped_release>());
-        m.def("cleanup_memory", &ArenaAllocator::cleanup, py::call_gaurd<py::gil_scoped_release>());
+        m.def("cleanup_memory", &ArenaAllocator::cleanup, py::call_guard<py::gil_scoped_release>());
 }
